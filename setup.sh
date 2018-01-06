d00d84734019e1165614a1c5185c0f03b3db593a
Yuan <zhangyuan@users.noreply.github.com>
2018-01-06 18:22:29 +0800

    create gocd server with 2 agents

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+mkdir -p volumes/agent01/godata/
+mkdir -p volumes/agent01/home/
+
+mkdir -p volumes/agent02/godata/
+mkdir -p volumes/agent02/home/
+
+mkdir -p volumes/server/godata/
+mkdir -p volumes/server/home/
